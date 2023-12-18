f9146048f13c2a8c0de4d11ea8e894f16e409af2
NurkaAmre <nurkerey30@gmail.com>
2023-12-18 20:26:00 +0600

    Implemented services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Button } from '@/components/UI/button'
 import CountryInfo from '@/components/countryInfo'
+import Services from '@/components/services'
 
 export default function Home() {
   return (
     <>
       <CountryInfo />
+      <Services />
     </>
   )
 }
