e79101001f3467fe4741e877cd8895a869420064
raihan2bd <raihan2bd.official@gmail.com>
2023-12-19 16:31:31 +0600

    Add Hero component into the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import { Button } from '@/components/UI/button'
 import CountryInfo from '@/components/countryInfo'
+import Hero from '@/components/sections/Hero'
 
 export default function Home() {
   return (
     <>
+      <Hero />
       <CountryInfo />
     </>
   )
