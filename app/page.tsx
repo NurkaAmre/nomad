83af3e11b02d31b9e92d52efcc41ff0451902cfd
raihan2bd <raihan2bd.official@gmail.com>
2023-12-20 17:46:31 +0600

    Add Popular section into the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import { Button } from '@/components/UI/button'
 import CountryInfo from '@/components/countryInfo'
 import Hero from '@/components/sections/Hero'
+import PopularPlaces from '@/components/sections/PopularPlaces'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <CountryInfo />
+      <PopularPlaces />
     </>
   )
 }
