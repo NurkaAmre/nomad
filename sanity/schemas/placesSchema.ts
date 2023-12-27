85f07ddd074b0a3a851d20db4923d696bb437c6b
raihan2bd <raihan2bd.official@gmail.com>
2023-12-27 16:25:16 +0600

    Update palces schema

--- sanity/schemas/placesSchema.ts
+++ sanity/schemas/placesSchema.ts
@@ -18,19 +18,25 @@ export const placesSchema =  {
       type: 'image',
       description: 'Upload the picture here',
     },
+    // {
+    //   name: 'category',
+    //   title: 'Category',
+    //   type: 'string',
+    //   description: 'Category of the picture (e.g., landscapes, beaches, cities, etc.)',
+    //   options: {
+    //     list: [ // Define the categories you want
+    //       { title: 'Landscapes', value: 'landscapes' },
+    //       { title: 'Beaches', value: 'beaches' },
+    //       { title: 'Cities', value: 'cities' },
+    //       // Add more categories as needed
+    //     ],
+    //   },
+    // },
     {
-      name: 'category',
-      title: 'Category',
-      type: 'string',
-      description: 'Category of the picture (e.g., landscapes, beaches, cities, etc.)',
-      options: {
-        list: [ // Define the categories you want
-          { title: 'Landscapes', value: 'landscapes' },
-          { title: 'Beaches', value: 'beaches' },
-          { title: 'Cities', value: 'cities' },
-          // Add more categories as needed
-        ],
-      },
-    },
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Description of the picture',
+    }
   ],
 };
