a057af48818d2f8e568cc11b438bdb5634cc69f0
raihan2bd <raihan2bd.official@gmail.com>
2024-01-05 20:44:05 +0600

    Add booking schema into the application

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -4,7 +4,8 @@ import account from "./account";
 import verificationToken from "./verificationToken";
 import tours from "./tours";
 import toursDesc from "./toursDesc";
+import booking from "./booking";
 
-const schemaTypes = [user, account, verificationToken, places, tours, toursDesc];
+const schemaTypes = [user, account, verificationToken, places, tours, toursDesc, booking];
 
 export default schemaTypes;
