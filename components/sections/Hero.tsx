b09572a8d0a51c8cb479a058f41af66e50193cec
NurkaAmre <nurkerey30@gmail.com>
2024-01-11 12:14:25 +0600

    Refactor design

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -20,16 +20,16 @@ const Hero = async () => {
           {heroPlacesList}
         </ul>
 
-        <div className='bg-[#2B2D4293] absolute top-[8rem] py-4 md:p-8 px-4 left-[6rem] '>
-          <p className='font-abhaya font-bold text-white md:text-6xl text-3xl tracking-[0.3rem] text-right'>Travel Beyond<br /> <span className="font-semibold text-5xl md:text-7xl text-[#f1683a] ">Limits</span><br /> with Nomads.</p>
+        <div className='bg-[#2B2D4293] rounded-md font-abril absolute top-[8rem] py-4 md:p-8 px-4 left-[6rem] '>
+          <p className='font-bold text-white md:text-6xl text-3xl tracking-[0.3rem] text-right'>Travel Beyond<br /> <span className="font-bold text-5xl md:text-7xl tracking-widest text-[#f1683a] ">Limits</span><br /> with Nomads.</p>
         </div>
 
       </div>
-      <div className='flex flex-col justify-center items-center bg-blend-overlay my-2 mx-4 py-4 px-6 md:mx-[12rem] rounded-sm text-white font-abhaya'>
-        <p className='md:text-3xl text-xl font-abhaya font-medium tracking-wider pb-3 md:px-[3rem] md:py-[2rem]'>
+      <div className='flex flex-col justify-center items-center bg-blend-overlay my-2 mx-4 py-4 px-6 md:mx-[12rem] rounded-sm text-white'>
+        <p className='md:text-3xl text-xl font-medium -tracking-wide pb-3 md:px-[3rem] md:py-[2rem]'>
           Start your travel at an affordable price with Nomads.Contact us not below.
         </p>
-        <button className='bg-[#6179ad] tracking-wide font-semibold px-4 py-2 md:text-2xl md:mb-5 hover:bg-[#93b7e3ec]'>
+        <button className='bg-[#6179ad] tracking-wide font-semibold uppercase px-4 py-2 md:text-2xl md:mb-5 hover:bg-[#93b7e3ec]'>
           Book Now
         </button>
       </div>
